da90f9071ab313d671a4bcd5420e46f5a4c4fc8f
nect88 <55210686+nect88@users.noreply.github.com>
2019-10-29 12:42:46 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,42 @@
 import socket
-from time import sleep
+import logging as log
+
+log.basicConfig(filename='client.txt', level=log.DEBUG)
+
+
+def send(conn, message):
+    header = len(message)
+    full_message = f'{header:4}{message}'.encode()
+    conn.send(full_message)
+
+
+def receive(conn):
+    header = conn.recv(4).decode()
+    message = conn.recv(int(header))
+    return message.decode()
+
+
+def identify(sock):
+    data = receive(sock)
+    if data == 'Как вас зовут?':
+        name = input(data)
+        send(sock, name)
+        data = receive(sock)
+    print(data)
+
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+log.info('Запуск клиента')
+port = int(input('port.py: '))
+sock.connect(('localhost', port))
+log.info('Подключен к серверу')
+identify(sock)
+msg = ''
+while msg != 'exit':
+    msg = input()
+    log.info('Отправка сообщения {}'.format(msg))
+    send(sock, msg)
+    log.info('Получение ответа от сервера')
+    received_msg = receive(sock)
+    print(received_msg)
+sock.close(
